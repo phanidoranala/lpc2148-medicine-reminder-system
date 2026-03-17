2ede58265627fb167e9a08fbcf467279a806b272
phanidoranala <phanidoranala@gmail.com>
2026-03-17 18:35:21 +0530

    Updated main_project.c

--- main_project.c
+++ main_project.c
@@ -1,83 +1,73 @@
-#include <lpc214x.h>
-#include "rtc.h"
-#include "lcd.h"
-#include "lcd_defines.h"
-#include "kpm_defines.h"
-#include "delay.h"
-#include "medicine.h"
-#include "config.h"
-#include "interrupt.h"
-#include "menu.h"
-
+#include <lpc214x.h>      // LPC2148 microcontroller register definitions
+#include "rtc.h"          // RTC related functions
+#include "lcd.h"          // LCD driver functions
+#include "lcd_defines.h"  // LCD command macros
+#include "kpm_defines.h"  // keypad definitions
+#include "delay.h"        // delay functions
+#include "medicine.h"     // medicine reminder logic
+#include "config.h"       // project configuration macros
+#include "interrupt.h"    // interrupt initialization and ISR declarations
+#include "menu.h"         // menu system functions
 
 // RTC variables used across modules
-s32 hour,min,sec;
-s32 date,month,year;
-s32 day;
-
+s32 hour,min,sec;        // variables to store current hour, minute, second
+s32 date,month,year;     // variables to store current date, month, year
+s32 day;                 // variable to store weekday value (0-6)
 // flags set by interrupt service routines
-int menu_flag = 0;   // set when SW1 is pressed
-int med_ack = 0;     // set when SW2 acknowledges medicine
-int last_min;
+int menu_flag = 0;       // set to 1 when SW1 interrupt requests menu
+int med_ack = 0;         // set to 1 when SW2 acknowledges medicine alert
+int last_min;            // stores previous minute value to detect minute change
+
 int main()
 {
-    RTC_Init();          // start RTC
-    Init_LCD();          // initialize LCD display
-    initKPM();           // initialize keypad
-    Interrupt_Init();    // configure SW1 and SW2 interrupts
-#ifndef SIMULATION
-		IODIR0 |= (1<<BUZZER_ALERT);
-    /*IODIR0 |= (1<<LED_ALERT);     // configure LED pin as output
-    IOSET0 = (1<<LED_ALERT);*/      // LED OFF (active low)
-#else
-   // IODIR0 |= (1<<BUZZER_ALERT);  // configure buzzer pin as output
+    RTC_Init();          // initialize RTC module and start time counting
+    Init_LCD();          // initialize LCD hardware and configure display
+    initKPM();           // initialize keypad pins for input
+    Interrupt_Init();    // configure external interrupts for SW1 and SW2
+
+#ifndef SIMULATION       // check if running on real hardware
+		IODIR0 |= (1<<BUZZER_ALERT);   // configure buzzer pin as output
+#else                    // if running in simulation mode
 	  IODIR0 |= (1<<LED_ALERT);     // configure LED pin as output
-    IOSET0 = (1<<LED_ALERT);      // LED OFF (active low)
+    IOSET0 = (1<<LED_ALERT);       // turn LED OFF initially (active low)
 #endif
-    while(1)
+
+    while(1)              // infinite loop for continuous system operation
     {
         // check if SW1 requested menu
-        if(menu_flag)
+        if(menu_flag)     // if interrupt set menu_flag
         {
-            menu_flag = 0;          // clear request
-
-            VICIntEnClr = (1<<14);  // disable SW1 interrupt temporarily
-
-            Menu_Setup();           // open configuration menu
-
-            Cmd_LCD(CLEAR_LCD);     // clear screen after menu exit
-
-            EXTINT = (1<<0);        // clear interrupt flag
-
+            menu_flag = 0;          // clear the flag
+            VICIntEnClr = (1<<14);  // temporarily disable SW1 interrupt
+            Menu_Setup();           // open configuration menu (time, medicine etc.)
+            Cmd_LCD(CLEAR_LCD);     // clear LCD after exiting menu
+            EXTINT = (1<<0);        // clear external interrupt flag
             VICIntEnable = (1<<14); // enable SW1 interrupt again
         }
+
         // read current RTC time
-        GetRTCTimeInfo(&hour,&min,&sec);
+        GetRTCTimeInfo(&hour,&min,&sec);   // get hour, minute, second from RTC registers
         // read current weekday
-        GetRTCDay(&day);
+        GetRTCDay(&day);                   // get day of week from RTC
         // read current date
-        GetRTCDateInfo(&date,&month,&year);
+        GetRTCDateInfo(&date,&month,&year); // get date, month, year from RTC
         // check if medicine reminder should run
-       if(CheckMedicineReminder(hour,min,&med_ack)==0)
+        if(CheckMedicineReminder(hour,min,&med_ack)==0)  // if no medicine alert active
 				{
-						DisplayRTCTime(hour,min,sec);
-						DisplayRTCDay(day);
-
+						DisplayRTCTime(hour,min,sec);   // display current time on LCD
+						DisplayRTCDay(day);              // display weekday next to time
 						// show NEXT MED only when minute changes
-						if(min != last_min)
+						if(min != last_min)             // check if minute value changed
 						{
-								last_min = min;
-
-								DisplayNextMedicine(hour,min);   // show next medicine
-								delay_ms(1000); 							// show for 1 second
-								Cmd_LCD(GOTO_LINE2_POS0+11);
-								Str_LCD("     ");
+							    last_min = min;           // update last recorded minute
+								DisplayNextMedicine(hour,min);   // show next medicine schedule
+								delay_ms(1000);                  // display message for 1 second
+								Cmd_LCD(GOTO_LINE2_POS0+11);     // move cursor to specific position
+								Str_LCD("     ");                // clear displayed medicine text
 						}
-
-						DisplayRTCDate(date,month,year);     // normal display
+						DisplayRTCDate(date,month,year);     // display date on LCD
 				}
-        // return 1 means medicine alert active
-        // so RTC display is skipped
+        // return value 1 means medicine alert is active
+        // so normal RTC display will be skipped
     }
 }
-
