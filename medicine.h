70c144e76fff90f80e08e84b2ab7a71bf90bd9ea
phanidoranala <phanidoranala@gmail.com>
2026-03-17 18:40:12 +0530

    Updated medicine.h

--- medicine.h
+++ medicine.h
@@ -1,7 +1,14 @@
-#ifndef MEDICINE_H
-#define MEDICINE_H
+#ifndef MEDICINE_H            // check if MEDICINE_H is already defined
+#define MEDICINE_H            // define MEDICINE_H to avoid multiple inclusion
 
-void SetMedicineSchedule(void);
+// function to configure medicine schedule using keypad
+void SetMedicineSchedule(void);   // allows user to enter medicine times
+// function to check if medicine reminder should trigger
+// hour ? current RTC hour
+// min  ? current RTC minute
+// med_ack ? pointer used to detect acknowledgement (SW2 press)
 int CheckMedicineReminder(int hour,int min,int *med_ack);
-void DisplayNextMedicine(int hour,int min);
-#endif
+// function to display next upcoming medicine time
+void DisplayNextMedicine(int hour,int min); // shows next medicine after current time
+
+#endif                        // end of header guard
