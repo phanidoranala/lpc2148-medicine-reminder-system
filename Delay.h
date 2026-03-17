74da339d8a0656b51cbe03574523b4b156a9035c
phanidoranala <phanidoranala@gmail.com>
2026-03-17 18:21:54 +0530

    Update Delay.h

--- Delay.h
+++ Delay.h
@@ -1,8 +1,10 @@
-#ifndef DELAY_H
-#define DELAY_H
+#ifndef DELAY_H              // check if DELAY_H is already defined
+#define DELAY_H              // define DELAY_H to prevent multiple inclusion
+// function prototype for microsecond delay
+void delay_us(unsigned int us);   // generates delay in microseconds
+// function prototype for millisecond delay
+void delay_ms(unsigned int ms);   // generates delay in milliseconds
+// function prototype for second delay
+void delay_s(unsigned int s);     // generates delay in seconds
+#endif                       // end of header guard
 
-void delay_us(unsigned int us);
-void delay_ms(unsigned int ms);
-void delay_s(unsigned int s);
-
-#endif
