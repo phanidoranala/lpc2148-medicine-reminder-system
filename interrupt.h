946f1447b454182637093956d7bfa694cd7e91cf
phanidoranala <phanidoranala@gmail.com>
2026-03-17 18:28:28 +0530

    Update interrupt.h

--- interrupt.h
+++ interrupt.h
@@ -1,10 +1,10 @@
-#ifndef INTERRUPT_H
-#define INTERRUPT_H
+#ifndef INTERRUPT_H            // check if INTERRUPT_H is already defined
+#define INTERRUPT_H            // define INTERRUPT_H to prevent multiple inclusion
 
-void Interrupt_Init(void);
-
-void eint0_isr(void) __irq;
-void eint1_isr(void) __irq;
-
-#endif
-
+// function prototype to initialize external interrupts
+void Interrupt_Init(void);    // configures SW1 and SW2 interrupt pins and VIC
+// interrupt service routine for EINT0
+void eint0_isr(void) __irq;   // executed when SW1 (P0.1) interrupt occurs
+// interrupt service routine for EINT1
+void eint1_isr(void) __irq;   // executed when SW2 (P0.3) interrupt occurs
+#endif                        // end of header guard
