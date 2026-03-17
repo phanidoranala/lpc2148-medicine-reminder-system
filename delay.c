83d67bb5dc59ca1b7f096ac3b9c8ebac607d7ae1
phanidoranala <phanidoranala@gmail.com>
2026-03-17 18:26:19 +0530

    Updated delay.c

--- delay.c
+++ delay.c
@@ -1,17 +1,13 @@
-#include "delay.h"
+#ifndef DELAY_H              // check if DELAY_H is already defined
+#define DELAY_H              // define DELAY_H to prevent multiple inclusion
 
-void delay_us(unsigned int us)
-{
-    for(us *= 12; us > 0; us--);
-}
+// function prototype for microsecond delay
+void delay_us(unsigned int us);   // generates delay in microseconds
+// function prototype for millisecond delay
+void delay_ms(unsigned int ms);   // generates delay in milliseconds
+// function prototype for second delay
+void delay_s(unsigned int s);     // generates delay in seconds
+#endif                       // end of header guard
 
-void delay_ms(unsigned int ms)
-{
-    for(ms *= 12000; ms > 0; ms--);
-}
 
-void delay_s(unsigned int s)
-{
-    for(s *= 12000000; s > 0; s--);
-}
 
